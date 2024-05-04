d11751f4587360938a1878ecf26751dbe8564aed
Gabriel Angelo B. Catimbang <75659838+gab-cat@users.noreply.github.com>
2024-05-04 06:06:58 +0000

    Update: Content changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full h-full cursor-pointer">
+    <main className="w-full h-full">
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
